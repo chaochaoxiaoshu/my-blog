e6b9aaecff4d9a0944dd59e638bb660a00c5e91b
张欣 <zhang.xin1@outlook.com>
2023-02-16 15:28:50 +0800

    update: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-import { header, menu, navigator, actions } from './header.module.scss'
+import {
+  header,
+  headerLogo,
+  burgerMenu,
+  active,
+  burgerMenuBar,
+  burgerMenuBarX,
+  navigator,
+  actions,
+  dropdown,
+  navigatorDropdown,
+  actionsDropdown,
+} from './header.module.scss'
 import Logo from '../images/svg/logo.svg'
-import Menu from '../images/svg/menu.svg'
 import Search from '../images/svg/search.svg'
 import Sun from '../images/svg/sun.svg'
 import Moon from '../images/svg/moon.svg'
+import { Dispatch, SetStateAction, useState } from 'react'
 
 const Navigator: React.FC = () => {
   return (
     <nav className={navigator}>
       <Link to='/'>Home</Link>
       <Link to='/blog'>Blog</Link>
-      <Link to='/blog'>Products</Link>
+      <Link to='/products'>Products</Link>
       <Link to='/about'>About</Link>
     </nav>
   )
@@ -37,22 +49,71 @@ const ThemePicker: React.FC<ThemePickerProps> = (props) => {
   )
 }
 
+interface ActiveProps {
+  isActive: boolean
+  setIsActive: Dispatch<SetStateAction<boolean>>
+}
+
+const BurgerMenu: React.FC<ActiveProps> = (props) => {
+  const { isActive, setIsActive } = props
+  return (
+    <div
+      className={`${isActive ? active : ''} ${burgerMenu}`}
+      onClick={() => setIsActive(!isActive)}
+    >
+      <span className={`${burgerMenuBar}`} />
+      <span className={`${burgerMenuBar}`} />
+      <span className={`${burgerMenuBar}`} />
+      <span className={`${isActive ? active : ''} ${burgerMenuBarX}`} />
+    </div>
+  )
+}
+
+interface DropdownProps extends ActiveProps {
+  useTheme: [string, (theme: string) => void]
+}
+
+const Dropdown: React.FC<DropdownProps> = (props) => {
+  const { isActive } = props
+  return (
+    <div className={`${isActive ? active : ''} ${dropdown}`}>
+      <nav className={`${navigatorDropdown}`}>
+        <Link to='/'>Home</Link>
+        <Link to='/blog'>Blog</Link>
+        <Link to='/products'>Products</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+      <div className={actionsDropdown}>
+        <ThemePicker useTheme={props.useTheme} />
+      </div>
+    </div>
+  )
+}
+
 interface HeaderProps {
   useTheme: [string, (theme: string) => void]
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
+  const [isActive, setIsActive] = useState(false)
   return (
     <header className={header}>
-      <Link to='/blog'>
+      <Link className={headerLogo} to='/'>
         <Logo />
       </Link>
+      {/* Desktop only */}
       <Navigator />
       <div className={actions}>
         <ThemePicker useTheme={props.useTheme} />
         <Search />
       </div>
-      <Menu className={menu} />
+      {/* Mobile only */}
+      <BurgerMenu isActive={isActive} setIsActive={setIsActive} />
+      <Dropdown
+        isActive={isActive}
+        setIsActive={setIsActive}
+        useTheme={props.useTheme}
+      />
     </header>
   )
 }
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const Products = () => {
+  return (
+    <Layout>
+      <p>Products Page</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title='Products' />
+
+export default Products
